56b51a04d14488403a863ea46656d8dbc024a4de
MatySykorka <sykoramatyas225@student.voskh.cz>
2025-03-03 14:02:33 +0100

    pages

---
+++ src/components/navBar/navBar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function NavBar() {
+        return (
+                <nav
+                    className="
+                        h-10
+                        gb-yellow-500
+                        w-full
+                    "
+                >
+                        <p>
+                            TestTEEEst
+                        </p>
+                </nav>
+        );
+}
